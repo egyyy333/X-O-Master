c5561540b4b26ff471f43d79752792c90a3a6584
egyyy333 <egyyy333@gmail.com>
2026-07-10 03:49:30 +0300

    test: update app name expectation
    Update the expected app name string in the test to match the localized string key "X-O Master".

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("إكس أو المحترف", appName)
+    assertEquals("X-O Master", appName)
   }
 }
